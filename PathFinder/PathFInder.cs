81c5f55453a6aaeb0a9fb2c2db67e73fc6bfc841
nam6711 <nam6711@g.rit.edu>
2022-11-11 22:20:41 -0500

    Added a small bug fix

--- PathFinder/PathFInder.cs
+++ PathFinder/PathFInder.cs
@@ -101,11 +101,11 @@ namespace PathFinder
     {
         // used to hold all Nodes loaded from a JSON file
         // public so that the x and y positions from JSON can be accessed outside the program
-        private SortedList<int, Node> nodes;
+        private SortedList<int, Node> nodes = new SortedList<int, Node>();
         // list that holds the weights of every node
         // Key - the id of a Node
         // Value - the weight of each connection the Node has
-        private SortedList<int, SortedList<int, double>> graph;
+        private SortedList<int, SortedList<int, double>> graph = new SortedList<int, SortedList<int, double>>();
 
         // Method: SetShortestDistance
         // Purpose: When given a set of Node id's and weights
