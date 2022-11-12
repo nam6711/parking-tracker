5afeb50e7a86e644552c81cac3ced05c11664799
nam6711 <nam6711@g.rit.edu>
2022-11-12 17:08:08 -0500

    removed an extra console log

--- PathFinderLibraryTestingProgram/PathFinderLibraryTestProgram/PathFinderLibraryTestProgram/Program.cs
+++ PathFinderLibraryTestingProgram/PathFinderLibraryTestProgram/PathFinderLibraryTestProgram/Program.cs
@@ -37,8 +37,7 @@ namespace PathFinderLibraryTestProgram
             PathCreate pc = new PathCreate("nodeList.json");
             // run the path finding method and store the output into a sorted list
             // for the given nodes, i've decided to path between nodes 1 to 4
-            Node[] path = pc.FindPath(1, 5);
-            Console.Write(path.Length);    
+            Node[] path = pc.FindPath(4, 7);   
             // iterate through the path, node by node. each node has a name, id, x, y value you can connect
             Console.WriteLine("******************************");
             Console.WriteLine("Step             Node");
