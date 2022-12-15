bd94457f4d577efa4005c34346cf892f038aa36c
clf8924 <111778040+clf8924@users.noreply.github.com>
2022-12-14 22:14:37 -0500

    WIP

---
+++ Fluke_RIT Parking Main Menu/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Fluke_RIT_Parking_Main_Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.displayLotsbutton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.copyrightLabel = new System.Windows.Forms.Label();
+            this.ritDiningButton = new System.Windows.Forms.Button();
+            this.scheduleEditorButton = new System.Windows.Forms.Button();
+            this.optimalLotButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.mainMenuButton = new System.Windows.Forms.Button();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Enabled = false;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.splitContainer1.Panel1.Controls.Add(this.displayLotsbutton);
+            this.splitContainer1.Panel1.Controls.Add(this.exitButton);
+            this.splitContainer1.Panel1.Controls.Add(this.copyrightLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.ritDiningButton);
+            this.splitContainer1.Panel1.Controls.Add(this.scheduleEditorButton);
+            this.splitContainer1.Panel1.Controls.Add(this.optimalLotButton);
+            this.splitContainer1.Panel1.Controls.Add(this.titleLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.mainMenuButton);
+            this.splitContainer1.Panel1.Enabled = false;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox);
+            this.splitContainer1.Size = new System.Drawing.Size(1064, 681);
+            this.splitContainer1.SplitterDistance = 282;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // displayLotsbutton
+            // 
+            this.displayLotsbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(105)))), ((int)(((byte)(2)))));
+            this.displayLotsbutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.displayLotsbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.displayLotsbutton.Font = new System.Drawing.Font("NeueHaasGroteskDisp Pro", 15.75F);
+            this.displayLotsbutton.ForeColor = System.Drawing.Color.White;
+            this.displayLotsbutton.Location = new System.Drawing.Point(18, 187);
+            this.displayLotsbutton.Name = "displayLotsbutton";
+            this.displayLotsbutton.Size = new System.Drawing.Size(241, 46);
+            this.displayLotsbutton.TabIndex = 7;
+            this.displayLotsbutton.Text = "Show All Lots";
+            this.displayLotsbutton.UseVisualStyleBackColor = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(105)))), ((int)(((byte)(2)))));
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("NeueHaasGroteskDisp Pro", 15.75F);
+            this.exitButton.ForeColor = System.Drawing.Color.White;
+            this.exitButton.Location = new System.Drawing.Point(18, 600);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(241, 46);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            // 
+            // copyrightLabel
+            // 
+            this.copyrightLabel.AutoSize = true;
+            this.copyrightLabel.Location = new System.Drawing.Point(8, 660);
+            this.copyrightLabel.Name = "copyrightLabel";
+            this.copyrightLabel.Size = new System.Drawing.Size(98, 13);
+            this.copyrightLabel.TabIndex = 5;
+            this.copyrightLabel.Text = "©Team 3  //  2022";
+            // 
+            // ritDiningButton
+            // 
+            this.ritDiningButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(105)))), ((int)(((byte)(2)))));
+            this.ritDiningButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ritDiningButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ritDiningButton.Font = new System.Drawing.Font("NeueHaasGroteskDisp Pro", 15.75F);
+            this.ritDiningButton.ForeColor = System.Drawing.Color.White;
+            this.ritDiningButton.Location = new System.Drawing.Point(18, 320);
+            this.ritDiningButton.Name = "ritDiningButton";
+            this.ritDiningButton.Size = new System.Drawing.Size(241, 46);
+            this.ritDiningButton.TabIndex = 4;
+            this.ritDiningButton.Text = "RIT Dining";
+            this.ritDiningButton.UseVisualStyleBackColor = false;
+            // 
+            // scheduleEditorButton
+            // 
+            this.scheduleEditorButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(135)))), ((int)(((byte)(142)))));
+            this.scheduleEditorButton.Enabled = false;
+            this.scheduleEditorButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scheduleEditorButton.Font = new System.Drawing.Font("NeueHaasGroteskDisp Pro", 15.75F);
+            this.scheduleEditorButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(211)))), ((int)(((byte)(212)))));
+            this.scheduleEditorButton.Location = new System.Drawing.Point(18, 389);
+            this.scheduleEditorButton.Name = "scheduleEditorButton";
+            this.scheduleEditorButton.Size = new System.Drawing.Size(241, 46);
+            this.scheduleEditorButton.TabIndex = 3;
+            this.scheduleEditorButton.Text = "Schedule Editor";
+            this.scheduleEditorButton.UseVisualStyleBackColor = false;
+            // 
+            // optimalLotButton
+            // 
+            this.optimalLotButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(105)))), ((int)(((byte)(2)))));
+            this.optimalLotButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.optimalLotButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.optimalLotButton.Font = new System.Drawing.Font("NeueHaasGroteskDisp Pro", 15.75F);
+            this.optimalLotButton.ForeColor = System.Drawing.Color.White;
+            this.optimalLotButton.Location = new System.Drawing.Point(18, 253);
+            this.optimalLotButton.Name = "optimalLotButton";
+            this.optimalLotButton.Size = new System.Drawing.Size(241, 46);
+            this.optimalLotButton.TabIndex = 2;
+            this.optimalLotButton.Text = "Find Optimal Lot";
+            this.optimalLotButton.UseVisualStyleBackColor = false;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("NeueHaasGroteskDisp Pro", 26F, System.Drawing.FontStyle.Bold);
+            this.titleLabel.ForeColor = System.Drawing.Color.Black;
+            this.titleLabel.Location = new System.Drawing.Point(8, 11);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(267, 84);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "RIT \r\nParking Tracker";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainMenuButton
+            // 
+            this.mainMenuButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(105)))), ((int)(((byte)(2)))));
+            this.mainMenuButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainMenuButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mainMenuButton.Font = new System.Drawing.Font("NeueHaasGroteskDisp Pro", 15.75F);
+            this.mainMenuButton.ForeColor = System.Drawing.Color.White;
+            this.mainMenuButton.Location = new System.Drawing.Point(18, 120);
+            this.mainMenuButton.Name = "mainMenuButton";
+            this.mainMenuButton.Size = new System.Drawing.Size(241, 46);
+            this.mainMenuButton.TabIndex = 0;
+            this.mainMenuButton.Text = "Main Menu";
+            this.mainMenuButton.UseVisualStyleBackColor = false;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.webBrowser1);
+            this.groupBox.Location = new System.Drawing.Point(3, 3);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(772, 675);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "RIT Interactive Map";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 16);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(766, 656);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://maps.rit.edu/", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "RIT Parking Tracker";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button mainMenuButton;
+        private System.Windows.Forms.Label copyrightLabel;
+        private System.Windows.Forms.Button ritDiningButton;
+        private System.Windows.Forms.Button scheduleEditorButton;
+        private System.Windows.Forms.Button optimalLotButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button displayLotsbutton;
+        private System.Windows.Forms.GroupBox groupBox;
+        public System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Fluke_RIT Parking Main Menu/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fluke_RIT_Parking_Main_Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.mainMenuButton.Click += new EventHandler(MainMenuButton__Click);
+            this.displayLotsbutton.Click += new EventHandler(DisplayLotsButton__Click);
+            this.optimalLotButton.Click += new EventHandler(OptimalLotButton__Click);
+            this.ritDiningButton.Click += new EventHandler(RITDiningButton__Click);
+            this.exitButton.Click += new EventHandler(ExitButton__Click);
+        }
+
+        private void MainMenuButton__Click(object sender, EventArgs e)
+        {
+            groupBox.Text = "RIT Interactive Map";
+            webBrowser1.Navigate("https://maps.rit.edu/");
+        }
+
+        private void DisplayLotsButton__Click(object sender, EventArgs e)
+        {
+            var myForm = new lotViewForm();
+            myForm.Show();
+        }
+        private void OptimalLotButton__Click(object sender, EventArgs e)
+        {
+
+        }
+        private void RITDiningButton__Click(object sender, EventArgs e)
+        {
+            groupBox.Text = "RIT Dining Hours and Availability";
+            webBrowser1.Navigate("https://www.rit.edu/fa/diningservices/places-to-eat/hours");
+        }
+        private void ExitButton__Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
